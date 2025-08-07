a129206a01e8bc49231a6aeb664fe769137fb424
Santos <jadson.santos@capgemini.com>
2025-08-07 12:36:10 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/cursojava/curso/entities/Category.java
+++ src/main/java/com/cursojava/curso/entities/Category.java
@@ -5,12 +5,14 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 
 
 @Entity
@@ -26,7 +28,8 @@ public class Category implements Serializable {
 	
 	private String name;
 	
-	@Transient
+	@JsonIgnore
+    @ManyToMany(mappedBy = "categories")       
 	private Set<Product> products = new HashSet<>();
 	
 	public Category() {
@@ -41,7 +44,7 @@ public class Category implements Serializable {
 		this.name = name;
 	}
 
-
+	
 
 	public Long getId() {
 		return id;
